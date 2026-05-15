035bbc909a1e852c5a433d1978406f2003ea51c7
Jean Paul Ferreira <jeanpaul@jepafe.net>
2026-05-14 21:49:15 -0300

    server express

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+
+const app = express();
+
+const products = [
+  { id: 1, name: "Producto 1", price: 10 },
+  { id: 2, name: "Producto 2", price: 20 },
+  { id: 3, name: "Producto 3", price: 30 },
+];
+
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  next()
+});
+
+app.get("/", (req, res) => {
+  res.send("Hola Mundo");
+});
+
+app.get("/products", (req, res) => {
+  res.send(products);
+});
+
+app.get("/products/:id", (req, res) => {
+  res.send(`Producto ${req.params.id}`);
+});
+
+app.use((req, res, next) => {
+  res.send("Not Found");
+});
+
+app.listen(3000, () => console.log("http://localhost:3000"));
